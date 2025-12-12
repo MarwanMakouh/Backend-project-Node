f958a78d8b3abf429fcbd29b66149fe92f2ff6f8
MarwanMakouh <marwan.makouh@student.ehb.be>
2025-12-12 16:18:43 +0100

    fix aan de connectie

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
 import app from "./app.js";
-import dotenv from "dotenv";
 import "./config/db.js";
-
-dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
